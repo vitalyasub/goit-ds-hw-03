1517955dddbb74444b19a57816b869201f3f652f
vitalyasub <vitalya.sub1213@gmail.com>
2025-06-04 16:55:38 +0300

    Done homework

--- task2/import_to_mongo.py
+++ task2/import_to_mongo.py
@@ -1,20 +1,17 @@
 import json
 from pymongo import MongoClient
 
-# Вставте сюди свій MongoDB URI
 MONGO_URI = "mongodb+srv://bugz:QZjizq56F-3vA@cluster0.8xtucnd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 
 client = MongoClient(MONGO_URI)
-db = client.quotes_db  # Назва вашої бази
+db = client.quotes_db
 
-# Завантаження з файлів
 with open("authors.json", encoding="utf-8") as af:
     authors = json.load(af)
 
 with open("quotes.json", encoding="utf-8") as qf:
     quotes = json.load(qf)
 
-# Імпорт у колекції
 db.authors.drop()
 db.quotes.drop()
 
